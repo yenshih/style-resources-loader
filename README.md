61273e7d2ceaf1f160d97779c41a7d0af911809e
yenshih <yenshih95@gmail.com>
2017-11-20 00:33:58 +0800

    Breaking changes! 0.3.0

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 [![npm version](https://img.shields.io/npm/v/style-resources-loader.svg?style=flat-square)](https://www.npmjs.com/package/style-resources-loader)
 [![npm downloads](https://img.shields.io/npm/dm/style-resources-loader.svg?style=flat-square)](https://www.npmjs.com/package/style-resources-loader)
 
-This loader is a CSS preprocessor resources loader for webpack, which inject your resources into multiple `sass / scss / less` files.
+This loader is a CSS preprocessor resources loader for webpack, which injects your style resources (e.g. `variables / mixins`) into multiple `sass / scss / less` files.
 
 It's mainly used to
  - share your `variables / mixins / functions` across all style files, so you don't need to `@import` them manually.
@@ -13,7 +13,7 @@ It's mainly used to
 
 ## Example
 
-**Prepend** `variables` and `mixins` to all `scss` files.
+Prepend `variables` and `mixins` to all `scss` files with default resources injector.
 
 ``` js
 {
@@ -21,7 +21,7 @@ It's mainly used to
     use: ['style-loader', 'css-loader', 'sass-loader', {
         loader: 'style-resources-loader',
         options: {
-            resources: [
+            patterns: [
                 'path/to/scss/variables/*.scss',
                 'path/to/scss/mixins/*.scss'
             ]
@@ -30,7 +30,7 @@ It's mainly used to
 }
 ```
 
-**Append** `variables` to all `less` files, override original `less variables`.
+Append `variables` to all `less` files and override original `less variables`.
 
 ```js
 {
@@ -38,8 +38,32 @@ It's mainly used to
     use: ['style-loader', 'css-loader', 'less-loader', {
         loader: 'style-resources-loader',
         options: {
-            resources: 'path/to/less/variables/*.less'
-            injector: 'append'
+            patterns: 'path/to/less/variables/*.less',
+            injector: (source, resources) => source + resources.map(({ content }) => content).join('\n')
+        }
+    }]
+}
+```
+
+Append `mixins` and prepend `variables` to all `scss` files with customized resources injector.
+
+``` js
+{
+    test: /\.scss$/,
+    use: ['style-loader', 'css-loader', 'sass-loader', {
+        loader: 'style-resources-loader',
+        options: {
+            patterns: [
+                'path/to/scss/variables/*.scss',
+                'path/to/scss/mixins/*.scss'
+            ],
+            injector: (source, resources) => {
+                const combineAll = (type) => resources
+                    .filter(({ file }) => file.includes(type))
+                    .map(({ content }) => content)
+                    .join('\n');
+                return combineAll('mixins') + source + combineAll('variables');
+            }
         }
     }]
 }
@@ -55,7 +79,36 @@ npm i style-resources-loader -D
 
 ### Loader Options
 
-|Name|Type|Default|Description|
-|:--:|:--:|:-----:|:----------|
-|**`resources`**|`{string \| string[]}`|`/`|Path to resources.|
-|**`injector`**|`{'prepend' \| 'append' \| (source: string, resource: string) => string}`|`'prepend'`|Indicate how the loader inject resources. You can write your own injector and control the resource injection precisely.|
+#### patterns
+
+A string or an array of string, represents the path to the resources you would like to inject.
+
+It supports globbing. You could include many files using a file mask.
+
+For example, `path.resolve(__dirname, './styles/*/*.less')` would include all `less` files from `variables` and `mixins` directories and ignore `reset.less` in such following structure.
+
+```
+./styles
+  /variables
+    |-- fonts.less
+    |-- colors.less
+  /mixins
+    |-- size.less
+  |-- reset.less
+```
+
+#### injector
+
+An optional function which controls the resources injection precisely.
+
+It receives two parameters:
+  - source
+    A string containing the content of the source file.
+  - resources
+    An array of resource, each contains `file` and `content` properties.
+    - file
+      A string represents the absolute path to the resource.
+    - content
+      A string containing the content of the resource file.
+
+It defaults to `(source, resources) => resources.map(({ content }) => content).join('\n') + source`, which means the loader prepends all resources to source file.
--- src/loader.ts
+++ src/loader.ts
@@ -1,80 +1,81 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import * as util from 'util';
 
 import * as glob from 'glob';
 import { loader } from 'webpack';
 import { getOptions } from 'loader-utils';
 
-export type StyleResourceInjectorKeys = 'prepend' | 'append';
+export interface StyleResource {
+    file: string;
+    content: string;
+}
 
-export type StyleResourcesInjector = (source: string | Buffer, resource: string) => string;
+export type StyleResources = ReadonlyArray<StyleResource>;
+
+export type StyleResourcesInjector = (source: string | Buffer, resources: StyleResources) => string;
 
 export type StyleResourcesLoaderOptions = Partial<{
-    resources: string | string[];
-    injector: StyleResourceInjectorKeys | StyleResourcesInjector;
+    patterns: string | string[];
+    injector: StyleResourcesInjector;
 }>;
 
 const loader: loader.Loader = function (source) {
     this.cacheable && this.cacheable();
 
     const callback = this.async();
+
     if (!callback) {
-        return this.callback(new Error('style-resources-loader: Asynchronous loader is not supported.'));
+        return this.callback(new Error('style-resources-loader: Asynchronous webpack loader is not supported.'));
     }
+
+    const defaultInjector: StyleResourcesInjector = (source, resources) =>
+        resources.map(({ content }) => content).join('\n') + source;
+
+    const { patterns, injector = defaultInjector }: StyleResourcesLoaderOptions = getOptions(this) || {};
 
     const isString = (arg: any): arg is string => typeof arg === 'string';
 
-    const { resources, injector = 'prepend' }: StyleResourcesLoaderOptions = getOptions(this) || {};
-
-    if (!isString(resources) && !(Array.isArray(resources) && resources.every(isString))) {
+    if (!isString(patterns) && !(Array.isArray(patterns) && patterns.every(isString))) {
         return callback(new Error(
-            'style-resources-loader: Expected options.resources to be a string or an array of string.'
-            + ` Instead received ${resources}: ${typeof resources}.`,
+            'style-resources-loader: Expected options.patterns to be a string or an array of string.'
+            + ` Instead received ${typeof patterns}: ${patterns}.`,
         ));
     }
 
-    if (isString(injector) ? !['prepend', 'append'].includes(injector) : typeof injector !== 'function') {
+    if (typeof injector !== 'function') {
         return callback(new Error(
-            'style-resources-loader: Expected options.resources to be `prepend`, `append` or a function.'
-            + ` Instead received ${injector}: ${typeof injector}.`,
+            'style-resources-loader: Expected options.injector to be a function.'
+            + ` Instead received ${typeof injector}: ${injector}.`,
         ));
     }
 
     const { context = process.cwd() } = this.options;
 
-    const files = (isString(resources)
-        ? glob.sync(resources)
-        : ([] as string[]).concat(...resources.map(resource => glob.sync(resource))))
-        .map(resource => path.resolve(context, resource));
+    const files = (isString(patterns) ? [patterns] : patterns)
+        .map(resource => glob.sync(resource).filter(file => /\.(?:s[ac]|le)ss$/i.test(file)))
+        .reduce((files, partialFiles) => files.concat(partialFiles))
+        .map(file => path.resolve(context, file));
 
     files.forEach(this.addDependency.bind(this));
 
-    Promise.all(files.map(async (file) => {
-        const content = await util.promisify(fs.readFile)(file, 'utf8');
-        if (!/\.(?:s[ac]|le)ss$/i.test(file)) {
-            return content;
-        }
-        return content.replace(/@import\s+(?:'([^'])+'|"([^"])"|([^\s"';]))/g, (match, single, double, unquote) => {
-            const absolutePath = path.resolve(path.dirname(file), single || double || unquote);
-            const relativePath = path.relative(this.context, absolutePath);
-            const quote = (match.match(/['"]$/) || [''])[0];
-            return `@import ${quote}${relativePath}${quote}`;
-        });
-    })).then((contents) => {
-        const resource = contents.join('\n');
-        if (typeof injector === 'function') {
-            try {
-                return callback(null, injector(source, resource));
-            } catch (err) {
-                return callback(err);
-            }
-        }
-        switch (injector) {
-            case 'prepend': return callback(null, resource + source);
-            case 'append': return callback(null, source + resource);
-        }
-    });
+    const resources: StyleResources = files.map(file => ({
+        file,
+        content: fs.readFileSync(file, 'utf8').replace(
+            /@import\s+(?:'([^'])+'|"([^"])"|([^\s"';]))/g,
+            (match, single, double, unquote) => {
+                const absolutePath = path.resolve(path.dirname(file), single || double || unquote);
+                const relativePath = path.relative(this.context, absolutePath);
+                const quote = (match.match(/['"]$/) || [''])[0];
+                return `@import ${quote}${relativePath}${quote}`;
+            },
+        ),
+    }));
+
+    try {
+        return callback(null, injector(source, resources));
+    } catch (err) {
+        return callback(err);
+    }
 };
 
 export default loader;
