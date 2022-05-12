e1fbe22ab6065624aa19531ccb98354692910f01
ben-lau <408736066@qq.com>
2022-05-12 17:13:33 +0800

    feat: support resolve url() statements

--- src/utils/resolve-import-url.ts
+++ src/utils/resolve-import-url.ts
@@ -1,23 +1,39 @@
+/* eslint-disable prefer-named-capture-group */
 import path from 'path';
 
 import type {LoaderContext, StyleResource} from '../types';
 
-/* eslint-disable-next-line prefer-named-capture-group */
-const regex = /@(?:import|require)\s+(?:\([a-z,\s]+\)\s*)?['"]?([^'"\s;]+)['"]?;?/gu;
+const regexImport = /@(?:import|require)\s+(?:\([a-z,\s]+\)\s*)?['"]?([^'"\s;]+)['"]?;?/gu;
+const regexUrl = /url\(['"]?([^'"\s;]+)['"]?\)/gu;
 
 export const resolveImportUrl = (ctx: LoaderContext, {file, content}: StyleResource): StyleResource => ({
     file,
-    content: content.replace(regex, (match: string, pathToResource?: string) => {
-        if (!pathToResource || /^[~/]/u.test(pathToResource)) {
-            return match;
-        }
+    content: content
+        .replace(regexImport, (match: string, pathToResource?: string) => {
+            if (!pathToResource || /^[~/]/u.test(pathToResource)) {
+                return match;
+            }
 
-        const absolutePathToResource = path.resolve(path.dirname(file), pathToResource);
-        const relativePathFromContextToResource = path
-            .relative(ctx.context, absolutePathToResource)
-            .split(path.sep)
-            .join('/');
+            const absolutePathToResource = path.resolve(path.dirname(file), pathToResource);
+            const relativePathFromContextToResource = path
+                .relative(ctx.context, absolutePathToResource)
+                .split(path.sep)
+                .join('/');
 
-        return match.replace(pathToResource, relativePathFromContextToResource);
-    }),
+            return match.replace(pathToResource, relativePathFromContextToResource);
+        })
+        .replace(regexUrl, (content: string, pathToResource?: string) => {
+            if (!pathToResource || /^(?:data:|~|\/)/u.test(pathToResource)) {
+                return content;
+            }
+
+            const absolutePathToResource = path.resolve(path.dirname(file), pathToResource);
+
+            const relativePathFromContextToResource = path
+                .relative(ctx.context, absolutePathToResource)
+                .split(path.sep)
+                .join('/');
+
+            return content.replace(pathToResource, relativePathFromContextToResource);
+        }),
 });
